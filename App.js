c0be825fe3a3613540b481608e1a34f034ea71e5
Gunwant WAnkhede <gunwant7272@gmail.com>
2023-11-04 19:06:49 +0530

    added auth components

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import MapViewComponent from './components/MapViewComponent';
+import { GluestackUIProvider, Text, Box, Button, ButtonText } from "@gluestack-ui/themed"
+import { config } from "@gluestack-ui/config" // Optional if you want to use default theme
+
 export default function App() {
   return (
-      <View style={styles.container}>
-        {/* <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" /> */}
-        <MapViewComponent/>
-      </View>
-  );
+    <GluestackUIProvider config={config}>
+      <Box width="100%" justifyContent="center" alignItems="center">
+        <Text>Open up App.js to start working on your app!</Text>
+        <MapViewComponent />
+        <Button>
+        <ButtonText>Hello world</ButtonText>
+      </Button>
+      </Box>
+    </GluestackUIProvider>
+  )
 }
 
 const styles = StyleSheet.create({
